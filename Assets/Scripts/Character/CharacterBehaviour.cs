802c4c949006f0ed2e41dde8e537c2267408ab49
trtrtrai <85724790+trtrtrai@users.noreply.github.com>
2022-08-20 21:03:06 +0700

    first commit

---
+++ Assets/Scripts/Character/CharacterBehaviour.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    [Range(0, 100)]
+    float speed;
+
+    [SerializeField]
+    [Range(0, 100)]
+    float jumpHigh;
+
+    [SerializeField]Rigidbody2D rigid;
+    [SerializeField]float maxSpeed;
+    [SerializeField]int jumpCount = 2;
+    [SerializeField]bool isGrounded;
+    [SerializeField]LayerMask ground;
+    [SerializeField]GameObject detectGround;
+    [SerializeField]new CircleCollider2D collider2D;
+    [SerializeField]Animator animator;
+
+    float jumpCD;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (isGrounded || jumpCount < 1)
+            {
+                //Debug.Log("Jump");
+                rigid.AddForce(transform.up * jumpHigh * Time.fixedDeltaTime, ForceMode2D.Impulse);
+                rigid.velocity = new Vector2(rigid.velocity.x, Vector2.ClampMagnitude(rigid.velocity, 5f).y);
+                jumpCount++;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {        
+        DetectGround();
+
+        animator.SetFloat("Yveloc", Mathf.Clamp(rigid.velocity.y, -1f, 4f));
+        var horizon = Input.GetAxis("Horizontal");
+        //Left-Right
+        rigid.AddForce(new Vector2(horizon * speed * Time.fixedDeltaTime, 0), ForceMode2D.Impulse);
+        rigid.velocity = new Vector2(Vector2.ClampMagnitude(rigid.velocity, maxSpeed).x, rigid.velocity.y);
+        if (horizon != 0) animator.SetBool("isRun", true);
+        else animator.SetBool("isRun", false);
+
+        //Flip
+        if (horizon > 0)
+        {
+            var scale = gameObject.transform.localScale;
+            scale.x = 1;
+
+            gameObject.transform.localScale = scale;
+        }
+        else if (horizon < 0)
+        {
+            var scale = gameObject.transform.localScale;
+            scale.x = -1;
+
+            gameObject.transform.localScale = scale;
+        }
+    }
+
+    void DetectGround()
+    {
+        if (Physics2D.CircleCast(collider2D.bounds.center, collider2D.bounds.extents.y, Vector2.down, 0.05f, ground).collider != null)
+        {
+            //Debug.Log("Detect ground");
+            isGrounded = true;
+            jumpCount = 0; 
+            jumpCD = Time.time + 0.2f;
+        }
+        else if (Time.time < jumpCD) isGrounded = true;
+        else isGrounded = false;
+
+        animator.SetBool("isJump", jumpCount == 0 && !isGrounded ? true : false);
+    }
+}
