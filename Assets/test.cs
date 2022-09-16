2004d5fcd4ac0705e7fb468169bdf7f9666768aa
trtrtrai <85724790+trtrtrai@users.noreply.github.com>
2022-09-16 22:34:46 +0700

    Set up URP

--- Assets/test.cs
+++ Assets/test.cs
@@ -7,11 +7,20 @@ using UnityEngine.UI;
 
 public class test : MonoBehaviour
 {
-     
+    [SerializeField]
+    Camera mainCmr;
     // Start is called before the first frame update
     void Start()
     {
-
+        var pos = mainCmr.transform.position;
+        var camSize = Camera.main.sensorSize;
+        var obj = Instantiate(Resources.Load<GameObject>("Prefabs/Image"), gameObject.transform);
+        pos.x += camSize.x * 0.4f / 10;
+        //pos.y = 0.5f;
+        pos.z = 0;
+        pos.x -= gameObject.transform.localPosition.x;
+        pos.y -= gameObject.transform.localPosition.y;
+        obj.GetComponent<RectTransform>().localPosition = pos;
     }
 
     // Update is called once per frame
